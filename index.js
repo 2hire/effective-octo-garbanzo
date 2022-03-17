f43d48de935f7bf39913084670dea3e27bf56053
Khalester <alessandro.natavio@gmail.com>
2022-03-17 11:09:58 +0100

    feat: testing fetch

--- index.js
+++ index.js
@@ -1,14 +1,21 @@
-const core = require('@actions/core');
-const github = require('@actions/github');
+const core = require("@actions/core");
+const github = require("@actions/github");
+const fetch = require("node-fetch");
 
 try {
   // `who-to-greet` input defined in action metadata file
-  const nameToGreet = core.getInput('who-to-greet');
+  const nameToGreet = core.getInput("who-to-greet");
   console.log(`Hello ${nameToGreet}!`);
-  const time = (new Date()).toTimeString();
+
+  const response = await fetch("https://api.github.com/users/github");
+  const data = await response.json();
+
+  console.log(data);
+
+  const time = new Date().toTimeString();
   core.setOutput("time", time);
   // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
+  const payload = JSON.stringify(github.context.payload, undefined, 2);
   console.log(`The event payload: ${payload}`);
 } catch (error) {
   core.setFailed(error.message);
