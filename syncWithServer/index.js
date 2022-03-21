0b20065430391455761ee13ada68624ba9fa420d
Khalester <alessandro.natavio@gmail.com>
2022-03-21 14:16:09 +0100

    feat: added sync with server action

--- syncWithServer/index.js
+++ syncWithServer/index.js
@@ -1,1 +1,30 @@
-console.log("Upload and do stuff");
+const core = require("@actions/core");
+const axios = require("axios");
+
+const getData = async (token) => {
+  try {
+    return await axios.get(
+      `https://raw.githubusercontent.com/Khalester/TestGithubActions/appName1/develop/settings/translations.json`,
+      {
+        headers: {
+          Authorization: `token ${token}`,
+        },
+      }
+    );
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+const main = async () => {
+  try {
+    const token = core.getInput("token");
+    const data = await getData(token);
+
+    core.setOutput('downloaded-translations', JSON.stringify(data, null, 2));
+  } catch (error) {
+    core.setFailed(error.message);
+  }
+}
+
+main();
