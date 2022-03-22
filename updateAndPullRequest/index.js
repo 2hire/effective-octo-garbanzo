ff7098d345226c7b983e68e88df95219985decf9
Khalester <alessandro.natavio@gmail.com>
2022-03-22 17:07:27 +0100

    refactor: refactoring

--- updateAndPullRequest/index.js
+++ updateAndPullRequest/index.js
@@ -2,36 +2,6 @@ const core = require("@actions/core");
 const axios = require("axios");
 const fs = require("fs");
 
-const sortObject = (object) => {
-  if (typeof object === "object" && !Array.isArray(object) && object !== null)
-    return Object.keys(object)
-      .sort()
-      .reduce((obj, key) => {
-        obj[key] = object[key];
-        return obj;
-      }, {});
-  return object;
-};
-
-const updateAndSortKeys = (source, target) => {
-  if (typeof source === "object" && !Array.isArray(source) && source !== null) {
-    const sourceKeys = Object.keys(source);
-    sourceKeys.forEach((key) => {
-      if (!target.hasOwnProperty(key)) target[key] = source[key];
-      else {
-        const [sKeys, tKeys] = updateAndSortKeys(source[key], target[key]);
-        source[key] = sKeys;
-        target[key] = tKeys;
-      }
-      // sorts children keys
-      source[key] = sortObject(source[key]);
-      target[key] = sortObject(target[key]);
-    });
-  }
-
-  return [source, target];
-};
-
 const getRawJsonData = async (owner, repo, branchName, path, token) => {
   try {
     return await axios.get(
@@ -47,110 +17,151 @@ const getRawJsonData = async (owner, repo, branchName, path, token) => {
   }
 };
 
-const getBranchRef = async (owner, repo, branchName, token) => {
-  try {
-    return await axios.get(
-      `https://api.github.com/repos/${owner}/${repo}/git/ref/heads/${branchName}`,
-      {
-        headers: {
-          Authorization: `token ${token}`,
-          Accept: "application/vnd.github.v3+json",
-        },
-      }
-    );
-  } catch (error) {
-    console.error(error);
-  }
+const Utils = {
+  sortObject: (object) => {
+    if (typeof object === "object" && !Array.isArray(object) && object !== null)
+      return Object.keys(object)
+        .sort()
+        .reduce((obj, key) => {
+          obj[key] = object[key];
+          return obj;
+        }, {});
+    return object;
+  },
+  updateAndSortKeys: (source, target) => {
+    if (
+      typeof source === "object" &&
+      !Array.isArray(source) &&
+      source !== null
+    ) {
+      const sourceKeys = Object.keys(source);
+      sourceKeys.forEach((key) => {
+        if (!target.hasOwnProperty(key)) target[key] = source[key];
+        else {
+          const [sKeys, tKeys] = updateAndSortKeys(source[key], target[key]);
+          source[key] = sKeys;
+          target[key] = tKeys;
+        }
+        // sorts children keys
+        source[key] = Utils.sortObject(source[key]);
+        target[key] = Utils.sortObject(target[key]);
+      });
+    }
+
+    return [source, target];
+  },
 };
 
-const createBranch = async (owner, repo, newBranchName, sha, token) => {
-  try {
-    return await axios.post(
-      `https://api.github.com/repos/${owner}/${repo}/git/refs`,
-      JSON.stringify({
-        ref: `refs/heads/${newBranchName}`,
-        sha,
-      }),
-      {
-        headers: {
-          Authorization: `token ${token}`,
-          Accept: "application/vnd.github.v3+json",
-        },
+const GitHubAPI = {
+  Branch: {
+    getRef: async (owner, repo, branchName, token) => {
+      try {
+        return await axios.get(
+          `https://api.github.com/repos/${owner}/${repo}/git/ref/heads/${branchName}`,
+          {
+            headers: {
+              Authorization: `token ${token}`,
+              Accept: "application/vnd.github.v3+json",
+            },
+          }
+        );
+      } catch (error) {
+        console.error(error);
       }
-    );
-  } catch (error) {
-    console.error(error);
-  }
-};
+    },
+    create: async (owner, repo, newBranchName, sha, token) => {
+      try {
+        return await axios.post(
+          `https://api.github.com/repos/${owner}/${repo}/git/refs`,
+          JSON.stringify({
+            ref: `refs/heads/${newBranchName}`,
+            sha,
+          }),
+          {
+            headers: {
+              Authorization: `token ${token}`,
+              Accept: "application/vnd.github.v3+json",
+            },
+          }
+        );
+      } catch (error) {
+        console.error(error);
+      }
+    },
+  },
 
-const getJsonFileInfo = async (owner, repo, branchName, path, token) => {
-  try {
-    return await axios.get(
-      `https://api.github.com/repos/${owner}/${repo}/contents/${path}?ref=${branchName}`,
-      {
-        headers: {
-          Authorization: `token ${token}`,
-          Accept: "application/vnd.github.v3+json",
-        },
+  Repository: {
+    getContents: async (owner, repo, branchName, path, token) => {
+      try {
+        return await axios.get(
+          `https://api.github.com/repos/${owner}/${repo}/contents/${path}?ref=${branchName}`,
+          {
+            headers: {
+              Authorization: `token ${token}`,
+              Accept: "application/vnd.github.v3+json",
+            },
+          }
+        );
+      } catch (error) {
+        console.error(error);
       }
-    );
-  } catch (error) {
-    console.error(error);
-  }
-};
+    },
+    updateContents: async (
+      owner,
+      repo,
+      branchName,
+      path,
+      content,
+      sha,
+      token
+    ) => {
+      try {
+        return await axios.put(
+          `https://api.github.com/repos/${owner}/${repo}/contents/${path}`,
+          JSON.stringify({
+            message: "[Garbanzo] Updated target json [skip ci]",
+            content: Buffer.from(
+              JSON.stringify(content, null, 2),
+              "utf-8"
+            ).toString("base64"),
+            sha,
+            branch: branchName,
+          }),
+          {
+            headers: {
+              Authorization: `token ${token}`,
+              Accept: "application/vnd.github.v3+json",
+            },
+          }
+        );
+      } catch (error) {
+        console.error(error);
+      }
+    },
+  },
 
-const updateJson = async (
-  owner,
-  repo,
-  branchName,
-  path,
-  content,
-  sha,
-  token
-) => {
-  try {
-    return await axios.put(
-      `https://api.github.com/repos/${owner}/${repo}/contents/${path}`,
-      JSON.stringify({
-        message: "[Garbanzo] Updated target json [skip ci]",
-        content: Buffer.from(
-          JSON.stringify(content, null, 2),
-          "utf-8"
-        ).toString("base64"),
-        sha,
-        branch: branchName,
-      }),
-      {
-        headers: {
-          Authorization: `token ${token}`,
-          Accept: "application/vnd.github.v3+json",
-        },
+  Pull: {
+    createRequest: async (owner, repo, head, base, token) => {
+      try {
+        return await axios.post(
+          `https://api.github.com/repos/${owner}/${repo}/pulls`,
+          JSON.stringify({
+            head,
+            base,
+            title: `[Garbanzo] Merging ${head} to ${base}`,
+          }),
+          {
+            headers: {
+              Authorization: `token ${token}`,
+              Accept: "application/vnd.github.v3+json",
+            },
+          }
+        );
+      } catch (error) {
+        console.error(error);
       }
-    );
-  } catch (error) {
-    console.error(error);
-  }
-};
-
-const createPullRequest = async (owner, repo, head, base, token) => {
-  try {
-    return await axios.post(
-      `https://api.github.com/repos/${owner}/${repo}/pulls`,
-      JSON.stringify({
-        head,
-        base,
-        title: `[Garbanzo] Merging ${head} to ${base}`,
-      }),
-      {
-        headers: {
-          Authorization: `token ${token}`,
-          Accept: "application/vnd.github.v3+json",
-        },
-      }
-    );
-  } catch (error) {
-    console.error(error);
-  }
+    },
+  },
 };
 
 const main = () => {
@@ -184,8 +195,11 @@ const main = () => {
           );
           if (responseData.data) {
             // Updates target keys
-            const [_, target] = updateAndSortKeys(source, responseData.data);
-            const responseBranchRef = await getBranchRef(
+            const [_, target] = Utils.updateAndSortKeys(
+              source,
+              responseData.data
+            );
+            const responseBranchRef = await GitHubAPI.Branch.getRef(
               owner,
               repo,
               branch,
@@ -193,8 +207,14 @@ const main = () => {
             );
             if (responseBranchRef.data) {
               const branchRefSHA = responseBranchRef.data.object.sha;
-              await createBranch(owner, repo, garbanzoBranch, branchRefSHA, token);
-              const responseJsonFile = await getJsonFileInfo(
+              await GitHubAPI.Branch.create(
+                owner,
+                repo,
+                garbanzoBranch,
+                branchRefSHA,
+                token
+              );
+              const responseJsonFile = await GitHubAPI.Repository.getContents(
                 owner,
                 repo,
                 garbanzoBranch,
@@ -203,7 +223,7 @@ const main = () => {
               );
               if (responseJsonFile.data) {
                 const jsonFileSHA = responseJsonFile.data.sha;
-                await updateJson(
+                await GitHubAPI.Repository.updateContents(
                   owner,
                   repo,
                   garbanzoBranch,
@@ -212,7 +232,13 @@ const main = () => {
                   jsonFileSHA,
                   token
                 );
-                await createPullRequest(owner, repo, garbanzoBranch, branch, token);
+                await GitHubAPI.Pull.createRequest(
+                  owner,
+                  repo,
+                  garbanzoBranch,
+                  branch,
+                  token
+                );
               }
             }
           }
