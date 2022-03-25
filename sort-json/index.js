a4725b76b20eeb3a08b214e324706b4eadf96474
Khalester <alessandro.natavio@gmail.com>
2022-03-25 17:42:43 +0100

    feat: changes

--- sort-json/index.js
+++ sort-json/index.js
@@ -1,5 +1,4 @@
 const core = require("@actions/core");
-const axios = require("axios");
 const fs = require("fs");
 
 const Utils = {
@@ -20,65 +19,9 @@ const Utils = {
   },
 };
 
-const GitHubAPI = {
-  Repository: {
-    getContents: async (owner, repo, branchName, path, token) => {
-      try {
-        return await axios.get(
-          `https://api.github.com/repos/${owner}/${repo}/contents/${path}?ref=${branchName}`,
-          {
-            headers: {
-              Authorization: `token ${token}`,
-              Accept: "application/vnd.github.v3+json",
-            },
-          }
-        );
-      } catch (error) {
-        console.error(error);
-      }
-    },
-    updateContents: async (
-      owner,
-      repo,
-      branchName,
-      path,
-      content,
-      sha,
-      token
-    ) => {
-      try {
-        return await axios.put(
-          `https://api.github.com/repos/${owner}/${repo}/contents/${path}`,
-          JSON.stringify({
-            message: "[Garbanzo] Sorted target json [skip ci]",
-            content: Buffer.from(
-              JSON.stringify(content, null, 2),
-              "utf-8"
-            ).toString("base64"),
-            sha,
-            branch: branchName,
-          }),
-          {
-            headers: {
-              Authorization: `token ${token}`,
-              Accept: "application/vnd.github.v3+json",
-            },
-          }
-        );
-      } catch (error) {
-        console.error(error);
-      }
-    },
-  },
-};
-
 const main = () => {
   try {
-    const owner = core.getInput("owner");
-    const repo = core.getInput("repo");
-    const currentBranchName = core.getInput("current-branch");
     const path = core.getInput("file-path");
-    const token = core.getInput("token");
 
     fs.readFile(path, "utf-8", async (error, data) => {
       if (error) {
@@ -86,25 +29,9 @@ const main = () => {
       }
       const object = JSON.parse(data);
 
-      const responseJsonFile = await GitHubAPI.Repository.getContents(
-        owner,
-        repo,
-        currentBranchName,
-        path,
-        token
-      );
-      if (responseJsonFile.data) {
-        const jsonFileSHA = responseJsonFile.data.sha;
-        await GitHubAPI.Repository.updateContents(
-          owner,
-          repo,
-          currentBranchName,
-          path,
-          Utils.sort(object),
-          jsonFileSHA,
-          token
-        );
-      }
+      fs.writeFile(path, sort(object), (error) => {
+        if (error) console.error(error);
+      });
     });
   } catch (error) {
     core.setFailed(error.message);
